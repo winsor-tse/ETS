e2c2609d7a5993592867b1d7bb602920bf5ea696
Winsor Tse <89112285+winsor-tse@users.noreply.github.com>
2022-12-08 23:34:25 -0500

    adding comments

--- Question_8_alter.py
+++ Question_8_alter.py
@@ -20,5 +20,84 @@ class Solution:
     """
     Time : N x N!
     Space: O(n!)
-    
-    """
+
+    not visited
+    1
+    not visited
+    2
+    not visited
+    3
+    max
+    [1, 2, 3]
+    popped
+    [1, 2]
+    popped
+    [1]
+    outer for loop -> 3
+    not visited
+    3
+    outer for loop when it hit 1 -> 2
+    not visited
+    2
+    max
+    [1, 3, 2]
+    outer for loop when it hit 1 -> 3 -> no change
+    popped
+    [1, 3]
+    outer for loop when it hit 3 -> null -> no change
+    popped
+    [1]
+    popped
+    []
+    outer for loop when it hit 2 -> 1
+    not visited
+    2
+    not visited
+    1
+    not visited
+    3
+    max
+    [2, 1, 3]
+    popped
+    [2, 1]
+    popped
+    [2]
+    not visited
+    3
+    not visited
+    1
+    max
+    [2, 3, 1]
+    popped
+    [2, 3]
+    popped
+    [2]
+    popped
+    []
+    not visited
+    3
+    not visited
+    1
+    not visited
+    2
+    max
+    [3, 1, 2]
+    popped
+    [3, 1]
+    popped
+    [3]
+    not visited
+    2
+    not visited
+    1
+    max
+    [3, 2, 1]
+    popped
+    [3, 2]
+    popped
+    [3]
+    popped
+    []
+
+        
+        """
